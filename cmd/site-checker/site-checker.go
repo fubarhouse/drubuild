60e79c1358d67de06e5c6f2a0cf6997cd8e11edf
Karl Hepworth <karl.hepworth@gmail.com>
2017-03-09 05:01:21 +0000

    add general purpose site checking utility.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ cmd/site-checker/site-checker.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	log "github.com/Sirupsen/logrus"
+	"github.com/fubarhouse/golang-drush/command"
+	"strings"
+)
+
+func main() {
+	var strAliases = flag.String("aliases", "", "alias1,alias2,alias3")
+	flag.Parse()
+
+	if *strAliases != "" {
+		for _, Alias := range strings.Split(*strAliases, "\n") {
+
+			cmdQ := command.NewDrushCommand()
+			cmdQ.SetAlias(Alias)
+			cmdQ.SetCommand("rr")
+			_, errQ := cmdQ.Output()
+			if errQ != nil {
+				log.Warnf("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			} else {
+				log.Infof("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			}
+
+			cmdR := command.NewDrushCommand()
+			cmdR.SetAlias(Alias)
+			cmdR.SetCommand("updb --yes")
+			_, errR := cmdQ.Output()
+			if errR != nil {
+				log.Warnf("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			} else {
+				log.Infof("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			}
+
+			cmdS := command.NewDrushCommand()
+			cmdS.SetAlias(Alias)
+			cmdS.SetCommand("cc all")
+			_, errS := cmdQ.Output()
+			if errS != nil {
+				log.Warnf("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			} else {
+				log.Infof("%v, %v, unsuccessful.\n", cmdQ.GetAlias(), cmdQ.GetCommand())
+			}
+
+		}
+	} else {
+		flag.Usage()
+	}
+}
