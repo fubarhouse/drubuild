b55b9c06fa355b771a7e584ccc4424356da53502
Karl Hepworth <karl.hepworth@gmail.com>
2017-03-09 08:07:51 +0000

    add verbose optional, add configurable commands.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/site-checker/site-checker.go
+++ cmd/site-checker/site-checker.go
@@ -2,48 +2,53 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/fubarhouse/golang-drush/command"
 	"strings"
 )
 
 func main() {
-	var strAliases = flag.String("aliases", "", "alias1,alias2,alias3")
+	var strAliases = flag.String("aliases", "", "comma-separated list of aliases for action")
+	var strCommands = flag.String("commands", "rr,updb --yes,cc all", "comma-separated list of commands for action")
+	var boolVerbose = flag.Bool("verbose", false, "adds raw output to end of program.")
 	flag.Parse()
 
+	var FinalOutput []string
+
 	if *strAliases != "" {
-		for _, Alias := range strings.Split(*strAliases, "\n") {
+		for _, Alias := range strings.Split(*strAliases, ",") {
 
-			cmdQ := command.NewDrushCommand()
-			cmdQ.SetAlias(Alias)
-			cmdQ.SetCommand("rr")
-			_, errQ := cmdQ.Output()
-			if errQ != nil {
-				log.Warnf("%v, %v, unsuccessful.", cmdQ.GetAlias(), cmdQ.GetCommand())
-			} else {
-				log.Infof("%v, %v, successful.", cmdQ.GetAlias(), cmdQ.GetCommand())
+			for _, Command := range strings.Split(*strCommands, ",") {
+
+				FinalOutput = append(FinalOutput, fmt.Sprintf("\n\ndrush @%v %v\n", Alias, Command))
+
+				DrushCommand := command.NewDrushCommand()
+				DrushCommand.SetAlias(Alias)
+				DrushCommand.SetCommand(Command)
+				DrushCommandOut, DrushCommandError := DrushCommand.Output()
+
+				if DrushCommandError != nil {
+					log.Warnf("%v, %v, unsuccessful.", DrushCommand.GetAlias(), DrushCommand.GetCommand())
+					StdOutLines := DrushCommandOut // strings.Split(string(DrushCommandOut), "\n")
+					for _, StdOutLine := range StdOutLines {
+						//log.Print(StdOutLine)
+						FinalOutput = append(FinalOutput, StdOutLine)
+					}
+				} else {
+					log.Infof("%v, %v, successful.", DrushCommand.GetAlias(), DrushCommand.GetCommand())
+					StdOutLines := DrushCommandOut // strings.Split(string(DrushCommandOut), "\n")
+					for _, StdOutLine := range StdOutLines {
+						//log.Print(StdOutLine)
+						FinalOutput = append(FinalOutput, StdOutLine)
+					}
+				}
 			}
-
-			cmdR := command.NewDrushCommand()
-			cmdR.SetAlias(Alias)
-			cmdR.SetCommand("updb --yes")
-			_, errR := cmdR.Output()
-			if errR != nil {
-				log.Warnf("%v, %v, unsuccessful.", cmdR.GetAlias(), cmdR.GetCommand())
-			} else {
-				log.Infof("%v, %v, successful.", cmdR.GetAlias(), cmdR.GetCommand())
+		}
+		if *boolVerbose {
+			for _, value := range FinalOutput {
+				log.Println(value)
 			}
-
-			cmdS := command.NewDrushCommand()
-			cmdS.SetAlias(Alias)
-			cmdS.SetCommand("cc all")
-			_, errS := cmdS.Output()
-			if errS != nil {
-				log.Warnf("%v, %v, unsuccessful.", cmdS.GetAlias(), cmdS.GetCommand())
-			} else {
-				log.Infof("%v, %v, successful.", cmdS.GetAlias(), cmdS.GetCommand())
-			}
-
 		}
 	} else {
 		flag.Usage()
