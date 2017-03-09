ef4b5b0112f79cf304d5b3350ba3b06b56f77127
Karl Hepworth <karl.hepworth@gmail.com>
2017-03-09 08:44:22 +0000

    Cleanup; Adds ability to add pattern modifier.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/site-checker/site-checker.go
+++ cmd/site-checker/site-checker.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	var strAliases = flag.String("aliases", "", "comma-separated list of aliases for action")
 	var strCommands = flag.String("commands", "rr,updb --yes,cc all", "comma-separated list of commands for action")
+	var strPattern = flag.String("pattern", "%v", "A modifier which allows rewriting of aliases replacing '%v' in the pattern with the alias.")
 	var boolVerbose = flag.Bool("verbose", false, "adds raw output to end of program.")
 	flag.Parse()
 
@@ -18,28 +19,23 @@ func main() {
 
 	if *strAliases != "" {
 		for _, Alias := range strings.Split(*strAliases, ",") {
-
+			Alias = strings.Replace(*strPattern, "%v", Alias, 1)
 			for _, Command := range strings.Split(*strCommands, ",") {
-
 				FinalOutput = append(FinalOutput, fmt.Sprintf("\n\ndrush @%v %v\n", Alias, Command))
-
 				DrushCommand := command.NewDrushCommand()
 				DrushCommand.SetAlias(Alias)
 				DrushCommand.SetCommand(Command)
 				DrushCommandOut, DrushCommandError := DrushCommand.Output()
-
 				if DrushCommandError != nil {
 					log.Warnf("%v, %v, unsuccessful.", DrushCommand.GetAlias(), DrushCommand.GetCommand())
-					StdOutLines := DrushCommandOut // strings.Split(string(DrushCommandOut), "\n")
+					StdOutLines := DrushCommandOut
 					for _, StdOutLine := range StdOutLines {
-						//log.Print(StdOutLine)
 						FinalOutput = append(FinalOutput, StdOutLine)
 					}
 				} else {
 					log.Infof("%v, %v, successful.", DrushCommand.GetAlias(), DrushCommand.GetCommand())
-					StdOutLines := DrushCommandOut // strings.Split(string(DrushCommandOut), "\n")
+					StdOutLines := DrushCommandOut
 					for _, StdOutLine := range StdOutLines {
-						//log.Print(StdOutLine)
 						FinalOutput = append(FinalOutput, StdOutLine)
 					}
 				}
