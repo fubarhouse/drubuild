b3448e126199f0204246bfcc3d4d8fe9d04f8944
Karl Hepworth <karl.hepworth@gmail.com>
2017-03-24 18:03:42 +1100

    Fixes #11.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/yoink-sync-site/main.go
+++ cmd/yoink-sync-site/main.go
@@ -5,12 +5,14 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/fubarhouse/golang-drush/command"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	var SourceAlias = flag.String("source-alias", "", "Alias of source site")
 	var DestAlias = flag.String("dest-alias", "", "Alias of destination site")
+	var Forbidden = flag.String("forbid", "", "For automation/security purposes, do not allow destination aliases to contain this string.")
 	var SyncDB = flag.Bool("db", false, "Mark database for syncronization")
 	var SyncFiles = flag.Bool("files", false, "Mark files for syncronization")
 
@@ -42,11 +44,19 @@ func main() {
 
 	if *SyncDB {
 		log.Infoln("Database was marked for syncing, working now...")
-		command.DrushDatabaseSync(*SourceAlias, *DestAlias)
+		if !strings.Contains(*DestAlias, *Forbidden) {
+			command.DrushDatabaseSync(*SourceAlias, *DestAlias)
+		} else {
+			log.Errorln("Destination alias contains a forbidden string")
+		}
 	}
 	if *SyncFiles {
 		log.Infoln("Files were marked for syncing, working now...")
-		command.DrushFilesSync(*SourceAlias, *DestAlias)
+		if !strings.Contains(*DestAlias, *Forbidden) {
+			command.DrushFilesSync(*SourceAlias, *DestAlias)
+		} else {
+			log.Errorln("Destination alias contains a forbidden string")
+		}
 	}
 	if *SyncDB || *SyncFiles {
 		log.Infoln("Attempting to rebuild registries...")
