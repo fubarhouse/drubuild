36ccf604bd2d85aa0d64664e5396342b039d8f22
Karl Hepworth <karl.hepworth@education.gov.au>
2017-06-26 15:01:28 +1000

    Adds binary for blocking users in bulk.
    Signed-off-by: Karl Hepworth <karl.hepworth@education.gov.au>

--- cmd/user-block/main.go
+++ cmd/user-block/main.go
@@ -1,1 +1,56 @@
-package user_block
+//
+// user-block will block all Drupal accounts on site alises based upon input values.
+//
+package main
+
+import (
+	"flag"
+	log "github.com/Sirupsen/logrus"
+	"github.com/fubarhouse/golang-drush/user"
+	"os"
+	"strings"
+)
+
+func main() {
+	var strAliases = flag.String("aliases", "", "comma-separated list of aliases for action")
+	var strPattern = flag.String("pattern", "%v", "A modifier which allows rewriting of aliases replacing '%v' in the pattern with the alias.")
+	var strUser = flag.String("user", "", "User name for blocking, example 'Firstname Sirname'")
+	flag.Parse()
+
+	if !strings.Contains(*strPattern, "%v") {
+		log.Errorln("Specified pattern does not include alias modifier.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *strUser == "" {
+		log.Errorln("User name is required and not specified.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *strAliases == "" {
+		flag.Usage()
+		log.Errorln("Aliases are not specified.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *strAliases != "" && *strUser != "" {
+		for _, Alias := range strings.Split(*strAliases, ",") {
+			Alias = strings.Replace(*strPattern, "%v", Alias, 1)
+			UserGroup := user.NewDrupalUserGroup()
+			UserGroup.Populate(Alias)
+			User := UserGroup.GetUser(*strUser)
+			if User.State == 1 {
+				User.State = 0
+				User.StateChange()
+			} else {
+				log.Infof("User '%v' is already blocked on %v", User.Name, Alias)
+			}
+		}
+	} else {
+		flag.Usage()
+		os.Exit(1)
+	}
+}
