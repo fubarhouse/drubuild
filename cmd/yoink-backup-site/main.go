254ff00daf705c8d48aa0a1d3c540f041401e453
Karl Hepworth <karl.hepworth@education.gov.au>
2017-07-05 11:05:51 +1000

    Add simple backup binary.

---
+++ cmd/yoink-backup-site/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	log "github.com/Sirupsen/logrus"
+	"github.com/fubarhouse/golang-drush/make"
+	"os"
+)
+
+func main() {
+
+	var Alias = flag.String("alias", "", "Alias of site")
+	var Destination = flag.String("destination", "/tmp/drush-ard.tar", "Destination file for backup.")
+
+	flag.Parse()
+
+	if *Alias == "" {
+		log.Infoln("Alias input is empty")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	x := make.NewSite("", "", "", *Alias, "", "", "", "")
+	x.ActionBackup(*Destination)
+}
