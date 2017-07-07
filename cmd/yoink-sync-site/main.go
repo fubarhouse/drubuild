369dbd0d31cb393633d27bc55c25c23df87a9ad0
Karl Hepworth <karl.hepworth@gmail.com>
2017-07-07 22:29:48 +1000

    Adjustments for Go Report
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/yoink-sync-site/main.go
+++ cmd/yoink-sync-site/main.go
@@ -9,6 +9,80 @@ import (
 	"sync"
 )
 
+// syncDatabase will begin the database sync task.
+func syncDatabase(SyncDB bool, SourceAlias, DestAlias, Forbidden string) {
+	if SyncDB {
+		log.Infoln("Database was marked for syncing, working now...")
+		if !strings.Contains(DestAlias, Forbidden) {
+			command.DrushDatabaseSync(SourceAlias, DestAlias)
+		} else {
+			log.Errorln("Destination alias contains a forbidden string")
+		}
+	}
+}
+
+// syncFiles will begin the file sync task.
+func syncFiles(SyncFiles bool, SourceAlias, DestAlias, Forbidden string) {
+	if SyncFiles {
+		log.Infoln("Files were marked for syncing, working now...")
+		if !strings.Contains(DestAlias, Forbidden) {
+			command.DrushFilesSync(SourceAlias, DestAlias)
+		} else {
+			log.Errorln("Destination alias contains a forbidden string")
+		}
+	}
+}
+
+// verifyFiles ensures the variables on the database for file system settings match the expected values.
+func verifyFiles(FilepathVerification bool, DestAlias, FilepathPublic, FilepathPrivate, FilepathTemporary string) {
+	if FilepathVerification {
+
+		// Go and find the path to prepend to paths...
+
+		y := command.NewDrushCommand()
+		y.Set(DestAlias, "status --format=var_export", false)
+		z, _ := y.Output()
+		var output string
+		for _, w := range z {
+			output += w
+		}
+		query := "'site' => '"
+		var actualResult string
+		outputLines := strings.Split(output, "\n")
+		for _, d := range outputLines {
+			if strings.Contains(d, query) {
+				d = strings.Replace(d, query, "", -1)
+				d = strings.Replace(d, ",", "", -1)
+				d = strings.Replace(d, "'", "", -1)
+				d = strings.Replace(d, " ", "", -1)
+				actualResult = d
+			}
+		}
+
+		type FSPath struct {
+			name  string
+			value string
+		}
+		FileSystemVars := []FSPath{
+			{"file_public_path", actualResult + "/" + FilepathPublic},
+			{"file_private_path", actualResult + "/" + FilepathPrivate},
+			{"file_temporary_path", actualResult + "/" + FilepathTemporary},
+		}
+		wg := sync.WaitGroup{}
+		for _, FileSystemVar := range FileSystemVars {
+			go func(FileSystemVar FSPath) {
+				wg.Add(1)
+				value := command.DrushVariableGet(DestAlias, FileSystemVar.name)
+				if value != FileSystemVar.value {
+					command.DrushVariableSet(DestAlias, FileSystemVar.name, FileSystemVar.value)
+				}
+				wg.Done()
+			}(FileSystemVar)
+			wg.Wait()
+		}
+	}
+}
+
 func main() {
 
 	var SourceAlias = flag.String("source-alias", "", "Alias of source site")
@@ -37,77 +111,18 @@ func main() {
 	}
 	if !*SyncDB {
 		log.Infoln("Database flag is switched off")
+	} else {
+		syncDatabase(*SyncDB, *SourceAlias, *DestAlias, *Forbidden)
 	}
 	if !*SyncFiles {
 		log.Infoln("Files flag is switched off")
+	} else {
+		syncFiles(*SyncFiles, *SourceAlias, *DestAlias, *Forbidden)
+		verifyFiles(*FilepathVerification, *DestAlias, *FilepathPublic, *FilepathPrivate, *FilepathTemporary)
 	}
-
 	if *SourceAlias == "" || *DestAlias == "" {
 		flag.Usage()
 		os.Exit(1)
-	}
-
-	if *SyncDB {
-		log.Infoln("Database was marked for syncing, working now...")
-		if !strings.Contains(*DestAlias, *Forbidden) {
-			command.DrushDatabaseSync(*SourceAlias, *DestAlias)
-		} else {
-			log.Errorln("Destination alias contains a forbidden string")
-		}
-	}
-	if *SyncFiles {
-		log.Infoln("Files were marked for syncing, working now...")
-		if !strings.Contains(*DestAlias, *Forbidden) {
-			command.DrushFilesSync(*SourceAlias, *DestAlias)
-		} else {
-			log.Errorln("Destination alias contains a forbidden string")
-		}
-	}
-	if *FilepathVerification {
-
-		// Go and find the path to prepend to paths...
-
-		y := command.NewDrushCommand()
-		y.Set(*DestAlias, "status --format=var_export", false)
-		z, _ := y.Output()
-		var output string
-		for _, w := range z {
-			output += w
-		}
-		query := "'site' => '"
-		var actualResult string
-		outputLines := strings.Split(output, "\n")
-		for _, d := range outputLines {
-			if strings.Contains(d, query) {
-				d = strings.Replace(d, query, "", -1)
-				d = strings.Replace(d, ",", "", -1)
-				d = strings.Replace(d, "'", "", -1)
-				d = strings.Replace(d, " ", "", -1)
-				actualResult = d
-			}
-		}
-
-		type FSPath struct {
-			name  string
-			value string
-		}
-		FileSystemVars := []FSPath{
-			{"file_public_path", actualResult + "/" + *FilepathPublic},
-			{"file_private_path", actualResult + "/" + *FilepathPrivate},
-			{"file_temporary_path", actualResult + "/" + *FilepathTemporary},
-		}
-		wg := sync.WaitGroup{}
-		for _, FileSystemVar := range FileSystemVars {
-			go func(FileSystemVar FSPath) {
-				wg.Add(1)
-				value := command.DrushVariableGet(*DestAlias, FileSystemVar.name)
-				if value != FileSystemVar.value {
-					command.DrushVariableSet(*DestAlias, FileSystemVar.name, FileSystemVar.value)
-				}
-				wg.Done()
-			}(FileSystemVar)
-			wg.Wait()
-		}
 	}
 	if *SyncDB || *SyncFiles {
 		log.Infoln("Attempting to rebuild registries...")
