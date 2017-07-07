369975c1e03bd8d5bbbd4f381b567514be375e6e
Karl Hepworth <Karl.Hepworth@education.gov.au>
2017-07-07 14:22:36 +1000

    Small bugfixes in the make updater; better comparrison for version checking.

--- makeupdater/makeupdater.go
+++ makeupdater/makeupdater.go
@@ -54,17 +54,17 @@ func UpdateMake(fullpath string) {
 	wg := sync.WaitGroup{}
 	for _, project := range projects {
 		go func(project string) {
-			wg.Add(1)
 			if project != "" {
+				wg.Add(1)
 				catCmd := "cat " + fullpath + " | grep \"projects\\[" + project + "\\]\" | grep version | cut -d '=' -f2"
 				z, _ := exec.Command("sh", "-c", catCmd).Output()
 				for _, stream := range strings.Split(string(z), "\n") {
-					stream = strings.Replace(stream, " ", "", -1)
 					stream = strings.Replace(stream, "\"", "", -1)
+					stream = strings.Replace(stream, " ", "", -1)
 					if stream != "" {
 						x, _ := exec.Command("sh", "-c", "drush pm-releases --pipe "+project+" | grep Recommended | cut -d',' -f2").Output()
 						versionNew := removeChar(string(x), " ", "7.x-", "\"", "\n", "[", "]")
-						if !strings.Contains(stream, versionNew) {
+						if stream != versionNew {
 							fmt.Printf("Replacing %v v%v with v%v\n", project, stream, versionNew)
 							replaceTextInFile(fullpath, fmt.Sprintf("projects[%v][version] = \"%v\"\n", project, stream), fmt.Sprintf("projects[%v][version] = \"%v\"\n", project, versionNew))
 							count++
