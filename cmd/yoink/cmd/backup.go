847bc6a64c5bbf4ec255f7186d11c08d367d89a5
Karl Hepworth <karl.hepworth@gmail.com>
2017-10-19 21:31:59 +1100

    Get the backup command working!
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ cmd/yoink/cmd/backup.go
@@ -0,0 +1,56 @@
+// Copyright © 2017 Karl Hepworth <Karl.Hepworth@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"os/exec"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"fmt"
+)
+
+var source string
+var destination string
+
+// backupCmd represents the backup command
+var backupCmd = &cobra.Command{
+	Use:   "backup",
+	Short: "Take a archive-dump snapshot of a local site",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if source == "" || destination == "" {
+			cmd.Usage()
+			fmt.Println("\nsource and/or destination are not set")
+			os.Exit(1)
+		}
+		d, err := exec.LookPath("drush")
+		if err != nil {
+			log.Fatal("Drush was not found in your $PATH")
+		}
+		c := exec.Command(d, source, "archive-dump", "--destination=" + destination)
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		c.Run()
+		c.Wait()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(backupCmd)
+	backupCmd.Flags().StringVarP(&source, "source", "s", "", "Drush alias to use for operation")
+	backupCmd.Flags().StringVarP(&destination, "destination", "d", "", "Path to Drush archive-dump destination")
+}
