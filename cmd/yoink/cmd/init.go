2f438f8e8099fc27895bd9f877ac03db9ebcb20a
Karl Hepworth <Karl.Hepworth@education.gov.au>
2017-11-15 11:40:18 +1100

    change default fastcgi_pass to PHP 7.1

--- cmd/yoink/cmd/init.go
+++ cmd/yoink/cmd/init.go
@@ -97,7 +97,7 @@ server {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         # fastcgi_pass  127.0.0.1:9000;
         fastcgi_index index.php;
-        fastcgi_pass unix:/var/run/php/php5.6-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_read_timeout 120;
     }
