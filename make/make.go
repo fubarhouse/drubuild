a231181d04fb0881ab2f04dd147dac612d59d668
Karl Hepworth <Karl.Hepworth@education.gov.au>
2017-11-15 12:46:10 +1100

    changes to exec.command for site-install.

--- make/make.go
+++ make/make.go
@@ -172,20 +172,18 @@ func (Site *Site) ActionInstall() {
 		panic(dbErr)
 	}
 	// Drush site-install
-	thisCmd := fmt.Sprintf("standard --yes --sites-subdir=%v --db-url=mysql://%v:%v@%v:%v/%v", Site.Name, Site.database.getUser(), Site.database.getPass(), Site.database.getHost(), Site.database.getPort(), dbName)
 	var sitePath string
 	sitePath = Site.Path + string(os.PathSeparator) + Site.Name + Site.Timestamp + string(os.PathSeparator) + Site.Docroot
 	d, e := exec.LookPath("drush")
 	if e != nil {
 		log.Fatalln(e)
 	}
-	i := exec.Command(d + " site-install", thisCmd)
+	i := exec.Command(d, "site-install", "standard", "--yes", "--sites-subdir="+Site.Name, fmt.Sprint("--db-url=mysql://%v:%v@%v:%v/%v", Site.database.getUser(), Site.database.getPass(), Site.database.getHost(), Site.database.getPort(), dbName))
 	i.Dir = sitePath
 	i.Stderr = os.Stderr
 	i.Stdout = os.Stdout
 	i.Run()
 	i.Wait()
-	log.Println(d, "site-install", thisCmd)
 }
 
 // ActionRebuildProject purges a specific project from a specified path, and re-download it
