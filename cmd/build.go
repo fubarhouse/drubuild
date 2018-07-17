2210f787f29f01f121123c146ba624150634ebe7
Karl Hepworth <karl.hepworth@gmail.com>
2018-07-17 21:40:02 +1000

    Rewrite build.go to match new commands and apis

--- cmd/build.go
+++ cmd/build.go
@@ -21,13 +21,9 @@ import (
 	"strings"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
-
-	"github.com/fubarhouse/drubuild/util/make"
-	c "github.com/fubarhouse/drubuild/util/composer"
-	"github.com/spf13/viper"
 	"path/filepath"
+	c "github.com/fubarhouse/drubuild/util/composer"
 )
 
 // removeGitFromPath will purge all .git data recursively from the specified path.
@@ -58,81 +54,32 @@ var buildCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		db_user = viper.GetString("db_user")
-		db_pass = viper.GetString("db_pass")
-		db_host = viper.GetString("db_host")
-		db_port = viper.GetInt("db_port")
+		timestamp, _ = strconv.ParseInt(time.Now().Format("20060102150405"), 0, 0)
+		c.Copy(composer, destination)
 
-		sites_php_template = viper.GetString("sites_php_template")
-
-		if docroot == "" {
-			log.Printf("docroot value is emptied, sub-folders will not be used.")
-			timestamp = 0
-		} else if timestamp == 0 {
-			timestamp, _ = strconv.ParseInt(time.Now().Format("20060102150405"), 0, 0)
-			log.Printf("Timestamp not specified, using %v", timestamp)
+		cargs := []string{"install", "-d="+destination}
+		if preferSource {
+			cargs = append(cargs, "--prefer-source")
 		}
-
-		x := make.NewSite(name, destination, alias, domain)
-		x.AliasTemplate = alias_template
-		x.Docroot = docroot
-		if alias == "" {
-			x.Alias = domain
-		}
-
-		y := make.NewmakeDB(db_host, db_user, db_pass, db_port)
-		x.DatabaseSet(y)
-
 		if workingCopy {
-			x.WorkingCopy = true
+			cargs = append(cargs, "--working-copy")
 		}
 
-		if composer != "" {
-			x.Composer = true
-			c.Copy(composer, x.Path)
-			c.Run([]string{"install", "-d="+ x.Path, "--working-copy", "--prefer-source"})
-		}
-
-		x.InstallSiteRef(sites_php_template)
-		if timestamp != 0 {
-			x.SymReinstall()
-		}
-
-		if installDrupal {
-			x.ActionInstall()
-		}
-
-		log.Infoln("Based upon the output above, you may need to restart the web service.")
+		c.Run(cargs)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(buildCmd)
-	// Required flags
-	buildCmd.Flags().StringVarP(&name, "name", "n", "", "The human-readable name for this site")
-	buildCmd.Flags().StringVarP(&alias, "alias", "a", "", "The drush alias for this site")
-	buildCmd.Flags().StringVarP(&destination, "destination", "p", "", "The path to where the site(s) exist.")
-	buildCmd.Flags().StringVarP(&domain, "domain", "d", "", "The domain this site is to use")
-	// Very important but not completely needed > 0 is needed though.
-	buildCmd.Flags().StringVarP(&composer, "composer", "c", "", "Path to the composer.json file.")
-	// Optional flags
-	buildCmd.Flags().StringVarP(&docroot, "docroot", "o", "docroot", "The folder to use for the built codebase.")
-	buildCmd.Flags().BoolVarP(&drupal, "drupal", "r", true, "Mark the build process as a Drupal build.")
-	buildCmd.Flags().BoolVarP(&installDrupal, "install", "i", false, "Mark this build so that installation doesn't happen.")
-	buildCmd.Flags().Int64VarP(&timestamp, "timestamp", "t", 0, "Optional timestamp in the format YYYYMMDDHHMMSS")
-	buildCmd.Flags().BoolVarP(&vhost, "vhost", "v", true, "Include a virtual host as configured with this build.")
+
+	// Parameters/Flags
+	buildCmd.Flags().StringVarP(&destination, "destination", "p", "", "The destination path")
+	buildCmd.Flags().StringVarP(&composer, "composer", "c", "", "Path to composer.json")
 	buildCmd.Flags().BoolVarP(&preferSource, "prefer-source", "s", false, "Build with preference to source packages.")
-	// Deprecated flags
-	buildCmd.Flags().BoolVarP(&workingCopy, "working-copy", "w", false, "Mark as a working-copy during the build process.")
-	// Hide deprecated fields.
-	buildCmd.Flags().MarkHidden("rewrite-source")
-	buildCmd.Flags().MarkHidden("rewrite-destination")
-	// Mark required flags.
+	buildCmd.Flags().BoolVarP(&workingCopy, "working-copy", "w", false, "Build with preference to working-copy")
+
+	// Required flags
 	buildCmd.MarkFlagRequired("name")
 	buildCmd.MarkFlagRequired("destination")
 	buildCmd.MarkFlagRequired("domain")
-
-	// Sites.php template
-	sites_php_template = viper.GetString("sites_php_template")
-
 }
